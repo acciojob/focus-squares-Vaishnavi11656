0d5e53fbbec49aed639b5199eb9b9fbc8a643b9d
Vaishnavi11656 <166276503+Vaishnavi11656@users.noreply.github.com>
2025-09-02 16:54:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+const square = document.querySelectorAll(".square")
+for(let square of squares){
+	square.addEventListener("mouseover",(e)=>addBgcolor(e))
+	square.addEventListener("mouseout",(e)=>addDefaultBgColor(e))													
+}
+
+function addBgcolor(){
+	const selectedBoxID = e.targert.id
+	for(let i =0;i<square.length;i++){
+		if(square[i].id !== selectedBoxID){
+			square[i],style.backgroundColor = "#6F4E37"
+		}
+	}
+}
+
+function addDefaultBgColor(){
+		for(let square of squares){
+			square.style.backgroundColor = "#E6E6FA"
+		}
+}
